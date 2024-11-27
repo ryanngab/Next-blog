3c97e746833194c706643a705831807bfa483638
ryanngab <firdausriyan402.com>
2024-11-27 10:00:54 +0700

    update forgot password

--- app/(cvcoders)/blog/_components/PinnedPost.tsx
+++ app/(cvcoders)/blog/_components/PinnedPost.tsx
@@ -15,6 +15,7 @@ interface Post {
   price: number;
   photo_url: string;
   pinned: boolean;
+  slug: string;
 }
 
 const PinnedPost: React.FC = () => {
@@ -92,7 +93,7 @@ const PinnedPost: React.FC = () => {
                     View Details
                   </Link>
 
-                  <ShareModalWithIcon />
+                  <ShareModalWithIcon url={`/products/${pinnedPost.slug}`} />
                 </div>
               </div>
             </div>
